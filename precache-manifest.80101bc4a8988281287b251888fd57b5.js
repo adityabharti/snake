746e946f47feba1e660d17411373bf42d024ccd9
adityabharti <aditya.9395@gmail.com>
2020-07-12 15:40:29 +0530

    Updates

---
+++ precache-manifest.80101bc4a8988281287b251888fd57b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3850105545bf427b4e72c5f07b518a6f",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "016d20996e567b8e251c",
+    "url": "/snake/static/css/main.b59102a0.chunk.css"
+  },
+  {
+    "revision": "80b5b411def5d33d90c8",
+    "url": "/snake/static/js/2.287bddfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.287bddfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016d20996e567b8e251c",
+    "url": "/snake/static/js/main.7475c13a.chunk.js"
+  },
+  {
+    "revision": "fab9d91fecf0babc8498",
+    "url": "/snake/static/js/runtime-main.608e748d.js"
+  }
+]);
