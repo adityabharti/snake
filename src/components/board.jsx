4aacc7e0f8f01bbfd0c6875748a9b309f1d9f6c6
adityabharti <aditya.9395@gmail.com>
2020-07-12 18:17:01 +0530

    Document the code and remove unnecessary comments

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -22,6 +22,10 @@ class Board extends Component {
   };
 
   isInsectOnSnake(snakeCoords, row, col) {
+    /**
+     * Check if the insect's co-ordinates co-incide with that of the snake.
+     * This method is called while generating insect's co-ordinates.
+     */
     for (let i = 0; i < snakeCoords.length; i++) {
       if (row === snakeCoords[i][0] && col === snakeCoords[i][1]) {
         return true;
@@ -31,6 +35,10 @@ class Board extends Component {
   }
 
   changeDirection = (event) => {
+    /**
+     * Upon an arrow key press, determine if the key press is valid and
+     * accordingly set the state.
+     */
     let prev_direction = this.state.direction;
     let direction = null;
 
@@ -50,27 +58,29 @@ class Board extends Component {
   };
 
   moveSnake() {
+    /**
+     * Entire logic to move the Snake.
+     */
     const snakeCo = [...this.state.snakeCoordinates];
     const griddy = [...this.state.grid];
     let highScore = this.state.highScore;
     let x, y;
     let randomRow, randomCol;
 
-    // var s = "";
-    // for (let i = 0; i < snakeCo.length; i++) {
-    //   s += snakeCo[i][0] + "-" + snakeCo[i][1] + "  ";
-    // }
-    // console.log(s);
-
+    // To allow the snake to move, remove the presence of the snake from
+    // old squares.
     for (let i = 0; i < snakeCo.length; i++) {
       griddy[snakeCo[i][0]][snakeCo[i][1]] = "";
     }
 
+    // Obtain the last co-ordinates to modify and set accordingly based on the
+    // direction.
     x = snakeCo[snakeCo.length - 1][0];
     y = snakeCo[snakeCo.length - 1][1];
+
+    // Tail of the snake is not needed anymore, remove it.
     const [old_coord_x, old_coord_y] = snakeCo.shift();
 
-    // console.log("x is ", x, " y is ", y);
     if (this.state.direction === RIGHT) {
       y += 1;
       snakeCo.push([x, y % COLS]);
@@ -86,7 +96,6 @@ class Board extends Component {
       x = x < 0 ? x + ROWS : x;
       snakeCo.push([x % ROWS, y]);
     }
-    //console.log("x is ", x, " y is ", y);
 
     // Check is insect is about to be eaten
     // If yes then generate a new insect
@@ -96,16 +105,12 @@ class Board extends Component {
       [randomRow, randomCol] = this.generateInsect(griddy, snakeCo);
       snakeCo.unshift([old_coord_x, old_coord_y]);
       highScore += 1;
-      console.log("High score is: ", this.state.highScore);
-
-      // document.removeEventListener("keydown", this.changeDirection, false);
-      // document.addEventListener("keydown", this.changeDirection, false);
-      // this.interval = setInterval(() => this.moveSnake(), MOVE_TIME_INTERVAL);
     } else if (isSnakeHeadOnBody(snakeCo)) {
       alert(`Game Over! Your score was: ${this.state.highScore}`);
       window.location.reload();
     }
 
+    // Update the grid with new snake co-ordinates.
     for (let i = 0; i < snakeCo.length; i++)
       griddy[snakeCo[i][0]][snakeCo[i][1]] = "S";
 
@@ -119,6 +124,14 @@ class Board extends Component {
   }
 
   componentDidMount() {
+    /**
+     * After the Board component has mounted:
+     * 1. Set the grid.
+     * 2. Initialise the default snake and lay it on the grid.
+     * 3. Generate the Insect's co-ordinates.
+     * 4. Set event listener on the 'document' to move the snake on arrow key
+     *    press.
+     */
     // Get default grid
     const grid = getGrid();
 
@@ -142,16 +155,23 @@ class Board extends Component {
     });
 
     document.addEventListener("keydown", this.changeDirection, false);
-
     this.interval = setInterval(() => this.moveSnake(), MOVE_TIME_INTERVAL);
   }
 
   componentWillUnmount() {
+    /**
+     * On Board component unmount, clear the interval and remove the event
+     * listener.
+     */
     clearInterval(this.interval);
     document.removeEventListener("keydown", this.changeDirection, false);
   }
 
   generateInsect(grid, snakeCoordinates) {
+    /**
+     * Generate the Insect.
+     */
+
     // Set the default random coordinates for the insect
     let [randomRow, randomCol] = getRandomRowCol();
 
@@ -164,13 +184,8 @@ class Board extends Component {
   }
 
   render() {
-    // const element = document.getElementsByTagName("html");
-    // console.log(element);
-    // element.addEventListener("keydown", this.changeDirection);
-    // console.log("entering render");
     const { grid } = this.state;
-    //console.log(this.state);
-    // console.log("render ", this.state.insectRow, " -- ", this.state.insectCol);
+
     return (
       <div className="grid">
         {grid.map((row, rowIdx) => {
